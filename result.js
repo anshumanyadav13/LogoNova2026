926e2a52119fbfc947ecfe5899d1ee1f06a9d875
anshumanyadav13 <anshumanyadav6119@gmail.com>
2026-07-11 12:17:46 +0530

    Add files via upload

---
+++ result.js
@@ -0,0 +1,35 @@
+let name = localStorage.getItem("name");
+let score = Number(localStorage.getItem("score"));
+let total = Number(localStorage.getItem("total"));
+
+document.getElementById("studentName").innerHTML = "👤 " + name;
+document.getElementById("score").innerHTML = score + " / " + total + " Points";
+
+let percent = Math.round((score / total) * 100);
+
+document.getElementById("percentage").innerHTML = percent + "%";
+
+let msg = "";
+
+if(percent >= 90){
+    msg = "🏆 Outstanding!";
+}
+else if(percent >= 75){
+    msg = "🌟 Excellent!";
+}
+else if(percent >= 60){
+    msg = "👏 Very Good!";
+}
+else if(percent >= 40){
+    msg = "🙂 Good Try!";
+}
+else{
+    msg = "💪 Keep Learning!";
+}
+
+document.getElementById("message").innerHTML = msg;
+
+function restartQuiz(){
+    localStorage.clear();
+    window.location.href = "index.html";
+}
